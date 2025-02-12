b1f0c60f67bb06b8827b145c5fcfa4ac37c2f10d
Karina <karina.tim98@gmail.com>
2025-02-12 20:54:58 +0300

    задачи 3 блока

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,24 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  
 
+  const splitPath = path.split('.');
+   
+  return function(obj) {
+
+    if (Object.keys(obj).length < 1) {
+      return; 
+    }
+    let returnValue; 
+    for (let i of splitPath) {
+      if (typeof obj[i] === 'object' && obj[i] !== null) {
+        obj = {...obj[i]};
+      }
+      else {
+        returnValue = obj[i];
+      }
+    }
+    return returnValue;
+  };
 }
